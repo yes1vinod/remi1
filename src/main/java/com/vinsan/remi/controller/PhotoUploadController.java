a437f251e271769b753a39fa491c811976c26ed0
yes1vinod <yes_vinod@yahoo.com>
2017-01-01 17:36:27 -0500

    Java Changes

---
+++ src/main/java/com/vinsan/remi/controller/PhotoUploadController.java
@@ -0,0 +1,42 @@
+package com.vinsan.remi.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.vinsan.remi.vo.FileUploadForm;
+
+@Controller
+public class PhotoUploadController {
+	@RequestMapping(value = "/show", method = RequestMethod.GET)
+	public String displayForm() {
+		return "file_upload_form";
+	}
+
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String save(@ModelAttribute("uploadForm") FileUploadForm uploadForm, Model map) {
+
+		List<MultipartFile> files = uploadForm.getFiles();
+
+		List<String> fileNames = new ArrayList<String>();
+
+		if (null != files && files.size() > 0) {
+			for (MultipartFile multipartFile : files) {
+
+				String fileName = multipartFile.getOriginalFilename();
+				fileNames.add(fileName);
+				// Handle file content - multipartFile.getInputStream()
+
+			}
+		}
+
+		map.addAttribute("files", fileNames);
+		return "file_upload_success";
+	}
+}
